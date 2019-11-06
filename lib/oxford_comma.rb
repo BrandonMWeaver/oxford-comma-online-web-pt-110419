39cf63755eceafb830a506611047a51aba83f076
Brandon Weaver <a7xncob@gmail.com>
2019-11-06 01:53:24 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if (array.size == 1)
+    return array[0]
+  end
+  
+  if (array.size == 2)
+    return "#{array[0]} and #{array[1]}"
+  end
+  
+  string = ""
+  index = 0
+  while index < array.size do
+    string += index != array.size - 1 ? "#{array[index]}, " : "and #{array[index]}"
+    index += 1
+  end
+  return string
 end
