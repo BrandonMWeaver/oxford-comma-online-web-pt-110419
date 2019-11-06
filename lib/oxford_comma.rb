3a34bfd066b6c90a039da6e98c7f5f8ef56962f1
Brandon Weaver <a7xncob@gmail.com>
2019-11-06 01:48:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
   string = ""
   index = 0
   while index < array.size do
-    string += index != array.size - 1 ? "#{array[index]}, " : "#{array[index]}"
+    string += index != array.size - 1 ? "#{array[index]}, " : "and #{array[index]}"
     index += 1
   end
   return string
